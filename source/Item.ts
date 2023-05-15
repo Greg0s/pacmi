af007f6306d179949f30a83382f272431fc7fb4a
Greg0s <gregoire.tinnes@outlook.fr>
2023-05-16 01:22:42 +0200

    add class items
    Co-authored-by: emmaveauvy <emmaveauvy@users.noreply.github.com>

--- source/Item.ts
+++ source/Item.ts
@@ -3,14 +3,14 @@ class Item extends Sprite {
     public scene_ : Scene;
     public ligne_ : number;
     public colonne_ : number;
-    public mort_ : boolean;
+    public used_ : boolean;
 
     public constructor(balise : HTMLElement, scene : Scene, ligne : number, colonne : number){
         super(balise);
         this.scene_= scene;
         this.ligne_ = ligne;
         this.colonne_ = colonne;
-        this.mort_=false;
+        this.used_=true;
     }
     
 }
