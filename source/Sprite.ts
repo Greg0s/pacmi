f0676606562b6ae52de5520caabc2623daf42bba
Greg0s <gregoire.tinnes@outlook.fr>
2023-05-29 11:11:43 +0200

    update sprite
    Co-authored-by: emmaveauvy <emmaveauvy@users.noreply.github.com>

--- source/Sprite.ts
+++ source/Sprite.ts
@@ -1,19 +1,5 @@
 //==================================================================================================
-// ANIMATION AVEC TYPESCRIPT                                                              Sprite.ts
-//                                                                               Par Bruno Bachelet
-//==================================================================================================
-// Copyright (c) 2017-2018
-// Bruno Bachelet - bruno@nawouak.net - http://www.nawouak.net
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 2 of the license, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details (http://www.gnu.org).
+//                                                                                         Sprite.ts
 
 // Classe  S p r i t e //---------------------------------------------------------------------------
 class Sprite {
